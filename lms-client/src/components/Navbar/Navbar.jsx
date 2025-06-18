485b26aa2291a8157d7ecd909d38e4f3779a19c3
Wahedul Islam <wahedulislam.pro@gmail.com>
2025-06-18 22:32:23 +0600

    setup the lms

---
+++ lms-client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+
+function Navbar() {
+  return (
+    <div className="flex justify-between py-3 px-20 border-b-2 border-[#eeeeee]">
+        {/* logo here */}
+        <h3>LearnHub</h3>
+
+        {/* menu here */}
+        <div className="flex gap-5">
+           <p>Home</p>
+           <p>all courses</p>
+        </div>
+
+        <div>
+            <p className="bg-[#0076df] text-white px-3">Login</p>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
